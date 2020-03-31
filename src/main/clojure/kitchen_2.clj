27868d3803e7bc3a7bf1a5e13182408033dbe13f
David John George <javajet@hotmail.com>
2020-03-31 14:45:49 +0100

    Update kitchen_2.clj

--- src/main/clojure/kitchen_2.clj
+++ src/main/clojure/kitchen_2.clj
@@ -1,20 +1,11 @@
 (ns kitchen-2)
 
-
 (defn create-drink [flavour] (conj [] flavour))
-
-
 (defn with-water [drink water] (conj drink water))
-
-
 (defn with-milk [drink milk] (conj drink milk))
-
-
 (defn with-sugar [drink sugar] (conj drink sugar))
 
-
 (defn stir [drink] (shuffle drink))
-
 
 (defn make-drink [tea water milk sugar]
   (-> (create-drink tea)
@@ -23,7 +14,4 @@
       (with-sugar sugar)
       (stir)))
 
-
 (defn -main [] (make-drink "tea" "water" "semi-skimmed" "one lump"))
-
-
