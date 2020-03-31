4129db313bda258b13d7ab3919338b36d421f852
David John George <javajet@hotmail.com>
2020-03-31 14:36:21 +0100

    Update example-1.scala

--- src/main/scala/kitchen/example-1.scala
+++ src/main/scala/kitchen/example-1.scala
@@ -1,24 +1,16 @@
 package kitchen
 
 /** Basic Example No Validation **/
-object Muggins1 extends App {
-
+object Example1 extends App {
 
   /*************************************/
 
-
   type Ingredient = String
-
   type Flavouring = Ingredient
-
   type Milk = Ingredient
-
   type Sugar = Ingredient
-
   type Water = Ingredient
-
   type Drink = Seq[Ingredient]
-
   type IngredientList = (Flavouring, Water, Milk, Sugar)
 
 
@@ -26,19 +18,12 @@ object Muggins1 extends App {
 
 
   val makeDrink: IngredientList => Option[Drink] = {
-
     case (flavour: Flavouring, water: Water, milk: Milk, sugar: Sugar) => for {
-
       step1 <- startMaking(flavour)
-
       step2 <- addWater(step1, water)
-
       step3 <- addMilk(step2, milk)
-
       drink <- addSugar(step3, sugar)
-
     } yield drink
-
   }
 
 
@@ -46,11 +31,8 @@ object Muggins1 extends App {
 
 
   val startMaking = (flavouring: Flavouring) => Some(Array(flavouring))
-
   val addWater = (drink: Drink, water: Water) => Some(drink :+ water)
-
   val addMilk = (drink: Drink, milk: Milk) => Some(drink :+ milk)
-
   val addSugar = (drink: Drink, water: Water) => Some(drink :+ water)
 
 
@@ -58,11 +40,8 @@ object Muggins1 extends App {
 
 
   for {
-
     drink <- makeDrink("Tea", "Hot Water", "Semi-skimmed Milk", "No sugar")
-
     msg <- drink mkString("Your drink contains: ", ", ", ".")
-
   } print (msg)
 
 
