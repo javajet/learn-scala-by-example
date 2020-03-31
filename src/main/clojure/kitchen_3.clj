f903d4bd23f1bed9b3532e7e6ae5823c4e9a850a
David John George <javajet@hotmail.com>
2020-03-31 14:46:22 +0100

    Update kitchen_3.clj

--- src/main/clojure/kitchen_3.clj
+++ src/main/clojure/kitchen_3.clj
@@ -1,32 +1,23 @@
 (ns kitchen-3)
 
-
 (defn create-drink [flavour] (conj [] flavour))
-
-
 (defn with-water
   [drink water]
   (if (clojure.string/includes? water "hot")
       (conj drink water)
       (nil)))
-
-
 (defn with-milk
   [drink milk]
   (if-not (clojure.string/includes? milk "sour")
           (conj drink milk)
           (nil)))
-
-
 (defn with-sugar
   [drink sugar]
   (if-not (clojure.string/includes? sugar "salt")
           (conj drink sugar)
           (nil)))
 
-
 (defn stir [drink] (shuffle drink))
-
 
 (defn make-drink [tea water milk sugar]
   (some-> (create-drink tea)
@@ -34,7 +25,6 @@
           (with-milk milk)
           (with-sugar sugar)
           (stir)))
-
 
 (defn -main []
   (do
