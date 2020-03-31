af846007ab0c9f9ee4a108f7a92ed2f428a16f4b
David John George <javajet@hotmail.com>
2020-03-31 14:41:20 +0100

    Update example-4.scala

--- src/main/scala/kitchen/example-4.scala
+++ src/main/scala/kitchen/example-4.scala
@@ -5,79 +5,46 @@ import scala.concurrent.duration._
 import scala.concurrent.ExecutionContext.Implicits.global
 
 /** Future Water & Milk **/
-object Muggins4 extends App {
-
+object Example4 extends App {
 
   /*************************************/
-
 
   type Ingredient = String
-
   type Flavouring = Ingredient
-
   type Milk = Ingredient
-
   type Sugar = Ingredient
-
   type Water = Ingredient
-
   type Drink = Seq[Ingredient]
-
   type IngredientList = (Flavouring, Future[Water], Future[Milk], Sugar)
-
   type DrinkResult = Future[Drink]
 
-
-
   /*************************************/
-
 
   case object BadMilk extends Exception("Bad Milk")
-
   case object ColdWater extends Exception("Cold Water")
-
   case object BadKettle extends Exception("Bad Kettle")
-
   case object InvalidSugar extends Exception("Invalid Sugar")
-
   case object NoMilk extends Exception("No Milk")
-
   val GONE_BAD = new Milk("Gone Bad")
-
   val INVALID_SUGAR = new Sugar("Salt")
 
-
   /*************************************/
-
 
   val makeDrink: IngredientList => DrinkResult = {
-
     case (flavour: Flavouring, boilWater: Future[Water], milkFromFridge: Future[Milk], sugar: Sugar) => for {
-
       water <- boilWater
-
       step1 <- startMaking(flavour)
-
       step2 <- addWater(step1, water)
-
       milk <- milkFromFridge
-
       step3 <- addMilk(step2, milk)
-
       drink <- addSugar(step3, sugar)
-
     } yield drink
-
   }
-
 
   /*************************************/
 
-
   val startMaking = (flavouring: Flavouring) => Future fromTry { Success(Array(flavouring)) }
-
   val addWater = (drink: Drink, water: Water) => Future fromTry {
-
     if ( water contains "Hot" )
       Success { drink :+ water }
     else
@@ -85,7 +52,6 @@ object Muggins4 extends App {
   }
 
   val addMilk = (drink: Drink, milk: Milk) => Future fromTry {
-
     if ( milk != GONE_BAD )
       Success { drink :+ milk }
     else
@@ -93,62 +59,42 @@ object Muggins4 extends App {
   }
 
   val addSugar = (drink: Drink, sugar: Sugar) => Future fromTry {
-
     if ( sugar != INVALID_SUGAR )
       Success { drink :+ sugar }
     else
       Failure { InvalidSugar }
   }
-
-
+  
   /*************************************/
-
 
   val boiledKettle: Future[Water] = Future { "Hot Water" }
-
   val stoppedKettle: Future[Water] = Future { "Cold Water" }
-
   val faultyKettle: Future[Water] = Future failed { BadKettle }
-
   val milkFromFridge: Future[Milk] = Future { "Semi-skimmed Milk" }
-
-  val outtaMilk: Future[Water] = Future failed { NoMilk }
-
+  val outOfMilk: Future[Water] = Future failed { NoMilk }
 
   /*************************************/
-
 
   Seq( (boiledKettle, milkFromFridge),
        (stoppedKettle, milkFromFridge),
        (faultyKettle, milkFromFridge),
-       (boiledKettle, outtaMilk) ) foreach {
-
+       (boiledKettle, outOfMilk) ) foreach {
     case (waitForKettle, waitForMilkman) =>
-
       Await ready (
-
         makeDrink ("Tea", waitForKettle, waitForMilkman, "No sugar")
-
         andThen {
-
           case Success(drink) =>
-
             println {
               drink mkString("Your drink contains: ", ", ", ".")
             }
-
           case Failure(e) =>
-
             println {
               f"Sadly, your drink wasn't made because: ${e getMessage}"
             }
         },
-
         5 seconds
       )
   }
 
-
   /*************************************/
-
 }
